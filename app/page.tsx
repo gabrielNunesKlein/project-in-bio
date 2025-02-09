0093c4255f5c3b3488df03ad76468d4e8cfa11a8
Gabriel Klein <gabriel.klein@evolutionsoft.com.br>
2025-02-09 19:22:40 -0300

    Feat: Create header

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
+import Header from "./components/leadingPage/header";
 import Hero from "./components/leadingPage/hero";
 
 export default function Home() {
   return (
     <div className="max-w-7xl mx-auto">
+      <Header />
       <Hero />
-      { /*<Header />
+      { /*
       <VideoExplanation />
       <Pricing />
       <FAQ /> */}
