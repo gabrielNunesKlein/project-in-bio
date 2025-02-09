cbda416acb5b0f9e945f3a5494dc8eadab99b838
Gabriel Klein <gabriel.klein@evolutionsoft.com.br>
2025-02-09 19:08:11 -0300

    Feat: project-card, total-visit component

--- app/components/commons/user-card.tsx
+++ app/components/commons/user-card.tsx
@@ -32,16 +32,15 @@ export default function UseCard() {
                     ))}
 
                 </div>
-
-                <div className='flex flex-col gap-3 w-full h-[172px]'>
-                    <div className='w-full flex flex-col items-center gap-3'>
-                        <Button className='w-full'>
-                            Template Saas - Compre Agora
-                        </Button>
-                        <button className='p-3 rounded-xl bg-[#1e1e1e] hover:bg-[#2e2e2e]'>
-                            <Plus />
-                        </button>
-                    </div>
+            </div>
+            <div className='flex flex-col gap-3 w-full h-[172px]'>
+                <div className='w-full flex flex-col items-center gap-3'>
+                    <Button className='w-full'>
+                        Template Saas - Compre Agora
+                    </Button>
+                    <button className='p-3 rounded-xl bg-[#1e1e1e] hover:bg-[#2e2e2e]'>
+                        <Plus />
+                    </button>
                 </div>
             </div>
         </div>
--- app/components/leadingPage/hero.tsx
+++ app/components/leadingPage/hero.tsx
@@ -1,10 +1,12 @@
+import ProjectCard from "../commons/project-card"
+import TotalVisit from "../commons/total-visit"
 import UseCard from "../commons/user-card"
 import Button from "../ui/button"
 import TextInput from "../ui/text-input"
 
 export default function Hero () {
     return (
-        <div className="flex h-screen">
+        <div className="flex h-screen gap-5">
             <div className="w-full flex flex-col gap-2 mt-[35vh]">
                 <h1 className="text-5xl font-bold text-white leading-[64px]">
                     Seus projetos e redes sociais em um único link
@@ -22,17 +24,17 @@ export default function Hero () {
                 </div>
             </div>
 
-            <div className="w-full items-center flex justify-center bg-[radial-gradient(circle_at_50%_50%,#402dbb,transparent_55%)]">
+            <div className="w-full flex items-center justify-center bg-[radial-gradient(circle_at_50%_50%,#4B2DBB,transparent_55%)]">
                 <div className="relative">
                     <UseCard />
                     <div className="absolute -bottom-[7%] -right-[45%]">
-                        { /* <TotalVisit /> */}
+                        <TotalVisit />
                     </div>
                     <div className="absolute top-[20%] -left-[45%] -z-10">
-                        { /* <ProjectCard /> */}
+                        <ProjectCard />
                     </div>
-                    <div className="absolute -top-[50%] -left-[55%] -z-10">
-                        { /* <ProjectCard /> */}
+                    <div className="absolute -top-[5%] -left-[55%] -z-10">
+                        <ProjectCard />
                     </div>
                 </div>
             </div>
