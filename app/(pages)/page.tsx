6ead1ba796b5e5f758719a73db48faeea993e977
Gabriel Klein <gabriel.klein@evolutionsoft.com.br>
2025-02-15 22:05:56 -0300

    Feat: page create link

---
+++ app/(pages)/page.tsx
@@ -0,0 +1,17 @@
+import FAQ from "../components/leadingPage/faq";
+import Header from "../components/leadingPage/header";
+import Hero from "../components/leadingPage/hero";
+import Pricing from "../components/leadingPage/pricing";
+import VideoExplanation from "../components/leadingPage/video-explanation";
+
+export default function Home() {
+  return (
+    <div className="max-w-7xl mx-auto">
+      <Header />
+      <Hero />
+      <VideoExplanation />
+      <Pricing />
+      <FAQ />
+    </div>
+  );
+}
